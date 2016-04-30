c901020ecabac1315d76dd180dc2d6df4c1efe5b
chmieleslav <chmieleslav@gmail.com>
2016-04-30 12:18:37 +0200

    some c# code exercises

---
+++ csharp/edx_AssignmentOne/edx_AssignmentOne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edx_AssignmentOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("First Name :");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Last Name :");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Birth Date :");
+            string birthDate = Console.ReadLine();
+
+            Console.WriteLine("Adress :");
+            string adress = Console.ReadLine();
+
+            Console.WriteLine("Your inputs :");
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(birthDate);
+            Console.WriteLine(adress);
+
+            Console.ReadLine();
+            */
+            Console.WriteLine("First Name :");
+            string firstName = Console.ReadLine();
+
+            foreach (char character in firstName)
+            {
+                Console.WriteLine(character);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ csharp/sbs_MyOwnApp/MyOwnApp/MyOwnApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwnApp
+{
+    class Circle
+    {
+        
+        public Circle()
+        {
+            radius = 0;
+        }
+
+        private double radius;
+
+
+        public void SetRadius(double r)
+        {
+            radius = r;
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+
+        public double calculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hi, man!");
+            //Console.ReadLine();
+
+            Circle circle1 = new Circle();
+            Console.WriteLine("At first, the radius is empty: ");
+            Console.WriteLine(circle1.GetRadius());
+
+            Console.WriteLine("Then, we modify the radius: ");
+            circle1.SetRadius(10);
+            Console.WriteLine(circle1.GetRadius());
+
+            Console.ReadLine();
+        }
+    }
+}
