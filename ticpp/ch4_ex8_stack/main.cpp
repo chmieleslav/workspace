f03b23510af18075467f60462114da35bdb0804e
chmieleslav <chmieleslav@gmail.com>
2016-04-30 12:29:14 +0200

    my recent exercises from Thinking In C++

---
+++ ticpp/ch4_ex8_stack/main.cpp
@@ -0,0 +1,38 @@
+/*
+Repeat Exercise 7 with  Stack. 
+
+[Ex. 7 was: Make a Stash that holds doubles. Fill it with 25 double
+values, then print them out to the console.]
+*/
+
+#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main (void)
+{
+	Stack stackForDoubles;
+	stackForDoubles.initialize();
+	stackForDoubles.
+	
+	 
+/*	
+	int initSize = 25;
+	CStash stashForDoubles;
+	initialize(&stashForDoubles, sizeof(double));
+	
+	double element = 1.123;
+	
+	for (int i = 0; i < initSize; i++)
+	{
+		add(&stashForDoubles, &element);
+		element = element + 0.0001;
+	}
+	
+	for (int i = 0; i < initSize; i++)
+	{
+		cout << "Element No. " << i << ":\t" << *(double*)fetch(&stashForDoubles, i) << endl; 
+	}
+*/
+}
